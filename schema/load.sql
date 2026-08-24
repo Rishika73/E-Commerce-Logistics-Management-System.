b0c8ab774831e7fecbdb631244d20fb2c15f2b86
Rishika73 <rishikareddythumma@gmail.com>
2026-08-23 22:59:10 -0400

    Add files via upload

---
+++ schema/load.sql
@@ -0,0 +1,8 @@
+copy users from '/Users/mahidharreddyindela/Documents/DMQL project/users.csv' delimiter ',' csv header;
+copy address from '/Users/mahidharreddyindela/Documents/DMQL project/address.csv' delimiter ',' csv header;
+copy category from '/Users/mahidharreddyindela/Documents/DMQL project/category.csv' delimiter ',' csv header;
+copy distribution_center from '/Users/mahidharreddyindela/Documents/DMQL project/distribution_centers.csv' delimiter ',' csv header;
+copy product from '/Users/mahidharreddyindela/Documents/DMQL project/products.csv' delimiter ',' csv header;
+copy inventory from '/Users/mahidharreddyindela/Documents/DMQL project/inventory_items.csv' delimiter ',' csv header;
+copy orders from '/Users/mahidharreddyindela/Documents/DMQL project/orders.csv' delimiter ',' csv header;
+copy order_items from '/Users/mahidharreddyindela/Documents/DMQL project/order_items.csv' delimiter ',' csv header;
